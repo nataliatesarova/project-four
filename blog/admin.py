1cccdaeacd2aff65f1455c84008dcc221d73945c
nataliatesarova <>
2023-09-18 13:12:45 +0000

    Registered the recipe model with the Django admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Recipe
+# nejsem si jista jestli to je recipe ve videu to je post
+admin.site.register(Recipe)
--- blog/models.py
+++ blog/models.py
@@ -9,18 +9,19 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Recipe(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts"
-    updated_on=models.DateTimeField(auto_now=True)
-    content=models.TextField()
-    featured_image=CloudinaryField('image', default='placeholder')
-    excerpt=models.TextField(blank=True)
-    created_on=models.DateTimeField(auto_now_add=True)
-    status=models.IntegerField(choices=STATUS, default=0)
-    likes=models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
     # Define the default ordering for recipes by creation date.
     class Meta:
-        ordering=['-created_on']
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
@@ -29,21 +30,23 @@ class Recipe(models.Model):
         return self.likes.count()
 
     # Create a Comment model.
-    class Comment(models.Model):
 
-        post=models.ForeignKey(
-            Post, on_delete=models.CASCADE, related_name='comments')
-        name=models.CharField(max_length=80)
-        email=models.EmailField()
-        body=models.TextField()
-        # DateTimeField named 'created_on' that automatically sets the current date and time when a new comment is created.
-        created_on=models.DateTimeField(auto_now_add=True)
-        # Create a boolean field named 'approved' with a default value of False which can be used to indicate if the comment has been approved or not.
-        approved=models.BooleanField(default=False)
 
-        # Comments will be sorted by ascending order (oldest comments first).
-        class Meta:
-        ordering=['created_on']
+class Comment(models.Model):
 
-        def __str__(self):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    # DateTimeField named 'created_on' that automatically sets the current date and time when a new comment is created.
+    created_on = models.DateTimeField(auto_now_add=True)
+    # Create a boolean field named 'approved' with a default value of False which can be used to indicate if the comment has been approved or not.
+    approved = models.BooleanField(default=False)
+
+    # Comments will be sorted by ascending order (oldest comments first).
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
         return f"Comment {self.body} by {self.name}"
