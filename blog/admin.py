07c63328c0cb4806edf635d6dd2f527c1b6bc448
nataliatesarova <>
2023-09-23 18:28:24 +0000

    Updated recipe admin configuration and added search functionality

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from .models import Recipe
 from django_summernote.admin import SummernoteModelAdmin
 
+
 class RecipeAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
 
 
 admin.site.register(Recipe, RecipeAdmin)
