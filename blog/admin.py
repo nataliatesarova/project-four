a03b3b8d7b5e000c32ea5ac9e403e7c58a22e12e
nataliatesarova <>
2023-09-23 18:11:30 +0000

    Added prepopulated fields for slug based on title in recipe admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from .models import Recipe
 from django_summernote.admin import SummernoteModelAdmin
 
-
-
 class RecipeAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
 
 
