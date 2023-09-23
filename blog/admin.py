b1dbce513ec52084520f5a7d3249a46938988cbc
nataliatesarova <>
2023-09-23 19:04:36 +0000

    Added comment approval functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -12,4 +12,20 @@ class RecipeAdmin(SummernoteModelAdmin):
     search_fields = ['title', 'content']
 
 
+# Register the Recipe model with the RecipeAdmin configuration
 admin.site.register(Recipe, RecipeAdmin)
+
+
+class AdminComment(admin.ModelAdmin):
+
+    list_filter = ('approved', 'created_on')
+    list_display = ('name', 'body', 'recipe', 'created_on', 'approved')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+ # Register the Comment model with the AdminComment configuration
+admin.site.register(Comment, AdminComment)
