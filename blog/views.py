03e4aefa0c53a4b34b06d3fcfb9d3aa6877fbf35
nataliatesarova <>
2023-10-03 19:24:32 +0000

    Edit RecipeList view function

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,16 @@ from django.views import generic
 from .models import Recipe
 
 
-class RecipeList(generic.ListView):
-    model = Recipe
-    queryset = Recipe.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
-    paginate_by = 9
+# class RecipeList(generic.ListView):
+#     model = Recipe
+#     queryset = Recipe.objects.filter(status=1).order_by("-created_on")
+#     template_name = "index.html"
+#     paginate_by = 9
+
+# view to display the recipes on index.html
+def RecipeList(request):
+    recipe_list = Recipe.objects.all()
+
+    return render(request, 'index.html', {'recipe_list' : recipe_list})
+
+
