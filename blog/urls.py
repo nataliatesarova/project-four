e8722daa5fe03a2a84f114b831be3a6e65e6a46c
nataliatesarova <>
2023-10-10 10:30:50 +0000

    Add route for creating new recipe

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.RecipeList, name='recipes'),
-    
+    # Create recipe
+    path('create_recipe/', views.create_recipe, name='create_recipe'),
+    # Recipe Detail
+    # path('recipe/<int:recipe_id>/', views.RecipeDetails, name='recipe_detail'),
 ]
