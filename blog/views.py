1dc68c0ffd73bde76ce98c89a461c54da8a7678f
nataliatesarova <>
2023-10-10 10:29:35 +0000

    Create view for new recipe

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
+from .forms import RecipeForm
 from .models import Recipe
+from django.contrib.auth.decorators import login_required
 
 
 # class RecipeList(generic.ListView):
@@ -10,9 +12,27 @@ from .models import Recipe
 #     paginate_by = 9
 
 # view to display the recipes on index.html
+
+
 def RecipeList(request):
     recipe_list = Recipe.objects.all()
 
-    return render(request, 'index.html', {'recipe_list' : recipe_list})
+    return render(request, 'index.html', {'recipe_list': recipe_list})
+
+# Creating a New Recipe
 
 
+@login_required
+def create_recipe(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            recipe.author = request.user
+            recipe.save()
+            # Redirect to the recipe detail page
+            return redirect('recipes')
+    else:
+        form = RecipeForm()
+
+    return render(request, 'blog/create_recipe.html', {'form': form})
