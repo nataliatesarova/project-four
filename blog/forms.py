1ef1828e33f5066c68eee8b1d07f53ca2311f761
nataliatesarova <>
2023-10-11 14:37:06 +0000

    Add slug field in recipe creation form

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Recipe
+
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['title','slug', 'content', 'featured_image', 'excerpt', 'status']
