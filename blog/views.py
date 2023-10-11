db6bf1c5de2114f95ce1ff439ff6dcba8dd92f2b
nataliatesarova <>
2023-10-11 16:05:47 +0000

    Create details and edit recipe view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from .forms import RecipeForm
 from .models import Recipe
@@ -35,4 +35,49 @@ def create_recipe(request):
     else:
         form = RecipeForm()
 
-    return render(request, 'blog/create_recipe.html', {'form': form})
+    return render(request, 'create_recipe.html', {'form': form})
+
+
+# View recipe details
+def RecipeDetails(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+
+    context = {
+        'recipe': recipe
+    }
+    return render(request, 'recipe_details.html', {'recipe': recipe})
+
+# Edit Recipe
+
+
+@login_required
+def EditRecipe(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+
+    if request.user != recipe.author:
+        pass
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe.save()
+            # Redirect to the recipe detail page
+            return redirect('recipe_detail', recipe_id=recipe.pk)
+        else:
+            form = RecipeForm(instance=recipe)
+        return render(request, 'blog/edit_recipe.html', {'form': form, 'recipe': recipe})
+
+
+# nevim jestli to je dobre
+
+
+# @login_required
+# def delete_recipe(request):
+#     if request.method == 'POST':
+#         form = DeleteRecipeForm(request.POST)
+#         if form.is_valid() and form.cleaned_data['confirmation']:
+#             recipe.delete()
+#             return redirect('recipes')
+#     else:
+#         form = DeleteRecipeForm()
+
+#     return render(request, 'delete_recipe.html', {'recipe': recipe, 'form': form})
