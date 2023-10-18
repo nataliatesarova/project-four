e3fa31f384e59cbfa11556faf199b248fca87957
nataliatesarova <>
2023-10-18 15:19:23 +0000

    Create a custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+# create a custom user
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.Charfield(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+
