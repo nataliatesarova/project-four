10e9f328368b9197fe5abdf9404c77113428793d
nataliatesarova <>
2023-10-18 15:21:48 +0000

    Create custom user and profile editing forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser, Profile
 
 # User registration form
 
 
-class RegistrationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2']
+        model = CustomUser
+        fields = ('username', 'first_name', 'last_name', 'email')
 
+# Editing user profile
+class EditProfileForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
 
-
+    class Meta:
+        model = Profile
+        fields = ['profile_picture', 'bio', 'first_name', 'last_name']
