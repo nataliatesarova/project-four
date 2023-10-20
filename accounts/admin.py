f22a08974ccbc6b642d8c9273fd6cc19a0184d23
nataliatesarova <>
2023-10-20 18:18:31 +0000

    Register custom user and profile fields for admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Profile, CustomUser
 # Register your models here.
+
+# Info to display on admin page
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+
+# Profile info
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'profile_picture', 'created_on', 'updated_on')
+    list_filter = ('created_on', 'updated_on')
+    search_fields = ('user__username', 'user__email')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Profile, ProfileAdmin)
