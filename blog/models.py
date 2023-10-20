54f38644b17284bcbfef64746a597843eaee4160
nataliatesarova <>
2023-10-20 18:44:29 +0000

    Fix recipe and comments classes

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
+from djang.conf import settings
+
 
 # Define possible status choices for the Recipe model.
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField("Title", max_length=200, unique=True)
+    slug = models.SlugField("Slug", max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="recipe_posts")
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="recipe_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('feature_image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    comments = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, through='Comment', related_name='recipe_comments')
     likes = models.ManyToManyField(
-        User, related_name='recipe_likes', blank=True)
+        settings.AUTH_USER_MODEL, related_name='blog_likes', blank=True)
 
     # Define the default ordering for recipes by creation date.
     class Meta:
@@ -27,23 +32,41 @@ class Recipe(models.Model):
     def __str__(self):
         return self.title
 
-    def number_of_likes(self):
+    def total_likes(self):
         return self.likes.count()
 
-    # Create a Comment model.
+    # Save slug
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Recipe, self).save(*args, **kwargs)
+
+
+# Comment status choices
+COMMENT_STATUS_CHOICES = [
+    ('pending', 'Pending Review'),
+    ('approved', 'Approved'),
+    ('rejected', 'Rejected'),
+]
+
+# Create a Comment model.
 
 
 class Comment(models.Model):
 
-    recipe = models.ForeignKey(
+    post = models.ForeignKey(
         Recipe, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+    text = models.TextField()
     # DateTimeField named 'created_on' that automatically sets the current date and time when a new comment is created.
     created_on = models.DateTimeField(auto_now_add=True)
     # Create a boolean field named 'approved' with a default value of False which can be used to indicate if the comment has been approved or not.
-    approved = models.BooleanField(default=False)
+    is_edited = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    status = models.CharField(
+        choices=COMMENT_STATUS_CHOICES, default='pending', max_lenghth=20)
 
     # Comments will be sorted by ascending order (oldest comments first).
     class Meta:
@@ -51,3 +74,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+    # Total comments
+    def total_comments(self):
+        return self.comments.count()
