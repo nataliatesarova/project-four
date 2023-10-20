f1b44c8ff32278c170b8a4fd4c122b35c08da221
nataliatesarova <>
2023-10-20 19:22:18 +0000

    Fix url paths for details, edit and delete

--- blog/urls.py
+++ blog/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     # Create recipe
     path('create_recipe/', views.create_recipe, name='create_recipe'),
     # Recipe Detail
-    path('recipe/<int:recipe_id>/', views.RecipeDetails, name='recipe_detail'),
+    path('recipe/<str:slug>/', views.recipe_detail, name='recipe_detail'),
     # Edit recipe
-    path('edit_recipe/<int:recipe_id>/', views.EditRecipe, name='edit_recipe'),
+    path('edit_recipe/<str:slug>/', views.EditRecipe, name='edit_recipe'),
     # Delete recipe
-    path('recipe/<int:recipe_id>/delete/',
+    path('delete_recipe/<str:slug>/delete/',
          views.delete_recipe, name='delete_recipe'),
     # Likes
     # path('recipe_like/<int:recipe_id>/',
