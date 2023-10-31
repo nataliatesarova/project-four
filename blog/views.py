2504098125a18c23bdc1797e924bb5b9a12f5dac
nataliatesarova <>
2023-10-31 15:15:08 +0000

    Add comment for block content in delete_recipe.html

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,6 @@ from django.contrib import messages
 def RecipeList(request):
     recipe_list = Recipe.objects.filter(status=1).order_by("-created_on")
     # Recipe Pages
-    # Recipe Pages
     recipes_per_page = 9
     paginator = Paginator(recipe_list, recipes_per_page)
     page_number = request.GET.get('page')
