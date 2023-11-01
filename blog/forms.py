2c54dfef3141a538003208e3e0f03b3c334d56b5
nataliatesarova <>
2023-11-01 17:48:27 +0000

    Rename content and context fields

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ from .models import Comment
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['title', 'content', 'featured_image', 'excerpt',
+        fields = ['title', 'description', 'ingredients', 'featured_image', 'method',
                   'status',]
 
 
