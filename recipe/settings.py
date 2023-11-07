f2f98a5b2958ce2be388825ae3a3031884d53b92
nataliatesarova <naftalintesarova@gmail.com>
2023-11-07 21:33:32 +0300

    add date formatter to project

--- recipe/settings.py
+++ recipe/settings.py
@@ -80,6 +80,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'blog.templatetags.recipe_filters', 
+            ],
         },
     },
 ]
