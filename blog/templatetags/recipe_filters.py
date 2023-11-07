51f58c7c90a78cddf0164a78f21c81fbf1195464
nataliatesarova <naftalintesarova@gmail.com>
2023-11-07 21:33:20 +0300

    add date formatter to project

--- blog/templatetags/recipe_filters.py
+++ blog/templatetags/recipe_filters.py
@@ -1,10 +1,26 @@
 from django import template
+from django.utils import timezone
 
 register = template.Library()
 
-# Truncate text length
+# Truncate Length of Text on Home Page
 @register.filter
 def truncate_text(value, max_length):
     if len(value) <= max_length:
         return value
     return value[:max_length] + ' . . .'
+
+# Date and time Filter for Comments
+@register.filter
+def format_comment_date(comment, recipe):
+    now = timezone.now()
+    comment_age = now - comment.created_on
+    
+    if comment_age.days < 0:
+        return comment.created_on.strftime("%F %d, %Y")
+    elif comment_age.days < 1:
+        return "Today"
+    elif comment_age.days == 1:
+        return "Yesterday"
+    else:
+        return comment.created_on.strftime("%b %d ")
