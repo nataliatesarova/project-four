828743373801da90bb7406ba0b1db8067ef2e2f6
nataliatesarova <119448458+nataliatesarova@users.noreply.github.com>
2023-11-12 20:46:55 +0000

    Update default profile picture

--- accounts/models.py
+++ accounts/models.py
@@ -23,7 +23,7 @@ class CustomUser(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    profile_picture = CloudinaryField('profile_picture', default='placeholder')
+    profile_picture = CloudinaryField('profile_picture', default='https://res.cloudinary.com/dxtdvo8ix/image/upload/v1699821909/uwmy2pkazv6ukhsknz3m.jpg')
     bio = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
