f8c366b4674b5dda927b152b8ba8d67db59853d5
nataliatesarova <naftalintesarova@gmail.com>
2023-11-14 22:50:00 +0300

    fix alignment on urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('login/', views.custom_login, name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('profile/<str:username>/', views.profile_details, name='profile'),
-    path('profile/update/<str:username>/',
-        views.update_profile, name='edit_profile'),
+    path('profile/update/<str:username>/', views.update_profile, name='edit_profile'),
 ]
