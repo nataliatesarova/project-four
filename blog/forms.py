71f1e1dce5e7258b96f0a1ed4ae30b97190cadfc
nataliatesarova <naftalintesarova@gmail.com>
2023-11-18 00:20:48 +0300

    Add filter tags in recipe creation form

--- blog/forms.py
+++ blog/forms.py
@@ -1,13 +1,27 @@
 from django import forms
 from .models import Recipe
 from .models import Comment
+from django_bleach.forms import BleachField
 
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['title', 'ingredients', 'description', 'method', 'featured_image', 'status']
-
+        fields = ['title', 'description', 'ingredients', 'method', 'featured_image', 'status']
+        
+    description = BleachField(
+        allowed_tags=['p', 'br', 'b', 'i', 'u', 'ul', 'li'],
+        allowed_attributes={'*': ['style']}
+    )
+    ingredients = BleachField(
+        allowed_tags=['p', 'br', 'b', 'i', 'u', 'ul', 'li'],
+        allowed_attributes={'*': ['style']}
+    )
+    
+    method = BleachField(
+        allowed_tags=['p', 'br', 'b', 'i', 'u', 'ul', 'li'],
+        allowed_attributes={'*': ['style']}
+    )
 
 class CommentForm(forms.ModelForm):
     class Meta:
